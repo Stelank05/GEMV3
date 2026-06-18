0c6b031c342d9a5eb1059b618ce99a4d5501e41c
Stelank05 <101483489+Stelank05@users.noreply.github.com>
2026-06-18 21:05:23 +0100

    V3.1 Code Files Upload Pt1

--- GEM Code V3/Properties/Resources.Designer.cs
+++ GEM Code V3/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GEM_Code_V3.Properties {
+namespace GEM_Code_V3_1.Properties {
     using System;
     
     
